47f9658de55f227bc30e515f71e19de07c6305b2
Ambreen Riaz <159724062+Ambreenriaz13@users.noreply.github.com>
2024-03-06 00:58:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var friendsName = ["saba", "sana", "laila", "ayesha"];
+friendsName.forEach(function (friendName) { return console.log("Hello ".concat(friendName, ", How are you")); });
---
+++ main.ts
@@ -0,0 +1,3 @@
+let friendsName = ["saba" ,"sana","laila","ayesha"];
+
+friendsName.forEach(friendName => console.log(`Hello ${friendName}, How are you`));
